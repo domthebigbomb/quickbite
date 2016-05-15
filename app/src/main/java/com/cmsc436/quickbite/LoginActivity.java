e3e075570b4c8e9998e413d004d3bd8c70a69a83
Yuval Cydulkin <ycydulkin@gmail.com>
2016-05-15 00:34:29 -0400

    logreg-polishing Polishes the log/reg activities to better match wireframe

--- app/src/main/java/com/cmsc436/quickbite/LoginActivity.java
+++ app/src/main/java/com/cmsc436/quickbite/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.Button;
--- app/src/main/java/com/cmsc436/quickbite/RegisterActivity.java
+++ app/src/main/java/com/cmsc436/quickbite/RegisterActivity.java
@@ -1,9 +1,13 @@
 package com.cmsc436.quickbite;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,8 +31,10 @@ public class RegisterActivity extends AppCompatActivity {
 
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPasswordConfirm = (EditText) findViewById(R.id.etPasswordConfirm);
 
         final TextView loginLink = (TextView) findViewById(R.id.etLoginHere);
+        final TextView tvIncError = (TextView) findViewById(R.id.tvIncError);
 
         // Sets up login link
         if (loginLink != null) {
@@ -49,7 +55,22 @@ public class RegisterActivity extends AppCompatActivity {
                     // Checks user/pass bounds
                     String username = etUsername.getText().toString();
                     String password = etPassword.getText().toString();
-                    if(username.length() > 0 && password.length() > 0) {
+                    String passwordConfirm = etPasswordConfirm.getText().toString();
+                    if(username.length() > 0 && password.length() > 0 && passwordConfirm.length() > 0) {
+
+                        if(!password.equals(passwordConfirm)) {
+                            // Shows the user an error message
+                            // Uses an Animation to fade in/out
+                            fadeTextView(tvIncError, TextView.VISIBLE);
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    fadeTextView(tvIncError, TextView.INVISIBLE);;
+                                }
+                            }, 4000);
+                        }
+
                         // Creates a new user
                         fb.createUser(username, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
                             @Override
@@ -65,10 +86,28 @@ public class RegisterActivity extends AppCompatActivity {
                             }
                         });
                     } else {
-                        // Have something fade in&out that explains user/pass bounds
                     }
                 }
             });
         }
+    }
+
+    public void fadeTextView(final TextView tv, int fadeTypeIn) {
+        final int fadeType = (fadeTypeIn==View.INVISIBLE) ? View.INVISIBLE : View.VISIBLE;
+        Animation fadeAnimation = (fadeType==View.INVISIBLE) ? new AlphaAnimation(1f, 0f) : new AlphaAnimation(0f, 1f);
+        fadeAnimation.setDuration(1300);
+        fadeAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                tv.setVisibility(fadeType);
+            }
+        });
+        tv.startAnimation(fadeAnimation);
     }
 }
