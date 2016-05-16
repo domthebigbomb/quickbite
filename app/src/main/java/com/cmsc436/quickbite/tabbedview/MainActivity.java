3090d1422eafd905dd5bbb8217fa58fd7f2ebbd7
Dominic Ong <dominic@driveshift.com>
2016-05-16 03:31:35 -0400

    Updated card layout to be closer to mocks

--- app/src/main/java/com/cmsc436/quickbite/tabbedview/MainActivity.java
+++ app/src/main/java/com/cmsc436/quickbite/tabbedview/MainActivity.java
@@ -9,29 +9,18 @@ import com.cmsc436.quickbite.R;
 import com.cmsc436.quickbite.slidingtab.slider.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private Toolbar toolbar;
     private ViewPager pager;
     private ViewPagerAdapter adapter;
     private SlidingTabLayout tabs;
     private CharSequence Titles[] = {"List", "Map"};
     int numOfTabs = 2;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
         getSupportActionBar().setElevation(0);
-//        setSupportActionBar(toolbar);
-//
-//
-//        getSupportActionBar().setTitle("Restaurants");
-
 
         adapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles,
                 numOfTabs);
@@ -42,12 +31,5 @@ public class MainActivity extends AppCompatActivity {
         tabs = (SlidingTabLayout) findViewById(R.id.tabs);
         tabs.setDistributeEvenly(true);
         tabs.setViewPager(pager);
-
-
     }
-
-
-
-
-
 }
